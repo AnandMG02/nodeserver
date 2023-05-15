991ab0477f62fc8935313c08816d3aaeabfbd444
Anand Mohan G <anand.mohan.g@ibm.com>
2023-05-15 16:12:30 +0530

    code is updated with externalip

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,21 @@
-# Use the official Node.js image as the parent image
-FROM node:latest
+# Use an official Node.js runtime as a parent image
+FROM node:16-alpine
 
 # Set the working directory to /app
 WORKDIR /app
 
-# Copy the package.json and package-lock.json files to the container
-COPY C:/REDHAT HACKATHON/nodejs/package*.json ./
+# Copy the package.json and package-lock.json files to the working directory
+COPY package*.json ./
 
-# Install the dependencies
+# Install app dependencies
 RUN npm install
+RUN npm install -g nodemon
 
-# Copy the rest of the application code to the container
-COPY C:/REDHAT HACKATHON/nodejs/ ./
+# Copy the rest of the application code to the working directory
+COPY . .
 
-# Expose port 3000 for the Node.js application
+# Expose the port that the app listens on
 EXPOSE 3000
 
-# Start the Node.js application
-CMD ["npm", "start"]
+# Define the command that starts the app
+CMD [ "npm", "start" ]
--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cluster = {
 const user = {
   name: 'IAM#anand.mohan.g@ibm.com',
   user: {
-    token: 'sha256~szBF9Q4VIDUPgoupQUsnvD7dmOYh9j-M1BRmB3IU7Q4',
+    token: 'sha256~tnA6Tx6jLcK1xl707ATaaXpMmMMkbUUR5ZxtR2LArHI',
   },
 };
 
@@ -36,30 +36,49 @@ const coreV1Api = kc.makeApiClient(k8s.CoreV1Api);
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/pods', (req, res) => {
-  coreV1Api.listNamespacedPod('hackathon2023-mongo-t-mobile')
-    .then((response) => {
-      const pods = response.body.items.map((pod) => ({
-        clustername: pod.metadata.name,
-        podip: pod.status.podIP,
-        podport: pod.spec.containers[0].ports[0].containerPort,
-        mongoUser: pod.spec.containers[0].env[0].value,
-        url: 'mongodb://' + pod.spec.containers[0].env[0].value + ':' + pod.spec.containers[0].env[1].value + '@' + pod.status.podIP + ':' + pod.spec.containers[0].ports[0].containerPort + '/?authSource=admin',
-        status: pod.status.phase
-      }));
-      console.log(pods);
-      res.json(pods);
-
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send({
-        message: 'Failed to get pods!',
-      });
-    });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
+
+app.get('/pods', async (req, res) => {
+  try {
+    console.log('Getting..');
+    const response = await coreV1Api.listNamespacedPod('hackathon2023-mongo-t-mobile');
+    const pods = await Promise.all(response.body.items.map(async (pod) => {
+      const podName = pod.metadata.name;
+      const podIP = await gettingexternalIP(podName, 'hackathon2023-mongo-t-mobile');
+      const podPort = pod.spec.containers[0].ports[0].containerPort;
+      const mongoUser = pod.spec.containers[0].env[0].value;
+      const url = `mongodb://${mongoUser}:${pod.spec.containers[0].env[1].value}@${podIP}:${podPort}/?authSource=admin`;
+      const status = pod.status.phase;
+
+      return {
+        clustername: podName,
+        podip: podIP,
+        podport: podPort,
+        mongoUser: mongoUser,
+        url: url,
+        status: status
+      };
+    }));
+
+    console.log(pods);
+    res.json(pods);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      message: 'Failed to get pods!',
+    });
+  }
+});
+
+
 app.post('/', (req, res) => {
+  console.log("Posting...");
   // Get the data from the request body
   const data = req.body
 
@@ -67,11 +86,13 @@ app.post('/', (req, res) => {
   const maxPort = 29999; // Maximum port number
   const randomMPort = Math.floor(Math.random() * (maxPort - minPort + 1)) + minPort;
 
-  // const minNPort = 30000; // Minimum port number
-  // const maxNPort = 32767; // Maximum port number
-  // const randomNPort = Math.floor(Math.random() * (maxPort - minPort + 1)) + minPort;
+  const minNPort = 30000; // Minimum port number
+  const maxNPort = 32767; // Maximum port number
+  const randomNPort = Math.floor(Math.random() * (maxNPort - minNPort + 1)) + minNPort;
   const availableMemSizeMB = 1024;
   const wiredTigerCacheSizeGB = Math.min(0.5, availableMemSizeMB / 1024);
+  const pvcName = 'mongodb-db';
+    const mountPath = '/data/db';
   
   // Define the pod template
   const pod = {
@@ -79,11 +100,41 @@ app.post('/', (req, res) => {
     kind: 'Pod',
     metadata: {
       name: data.cluster,
-    },
-    spec: {
-      securityContext: {
-        runAsNonRoot: true,
+      labels: {
+        "deployment": "mongo-db",
+        "pod-template-hash": "67df6dd788"
       },
+      
+      ownerReferences: [
+        {
+          apiVersion: 'v1',
+          kind: 'Deployment', // Update with the appropriate owner kind
+          name: 'mongo-db', // Update with the actual MongoDB resource name
+          uid: '250f672c-97dc-46d2-8fa1-3d2731d6c537', // Update with the actual MongoDB resource UID
+          controller: true,
+        },
+      ],
+      securityContext: {
+        "allowPrivilegeEscalation": false,
+        "capabilities": {
+            "drop": [
+                "ALL"
+            ]
+        },
+        "runAsNonRoot": true,
+        "runAsUser": 1001000000
+    },
+        },
+    spec: {
+      volumes: [
+        {
+          name: 'mongodb-storage',
+          persistentVolumeClaim: {
+            claimName: pvcName,
+          },
+        },
+      ],
+      // Rest of the pod configuration remains unchanged
       containers: [
         {
           name: 'mongo',
@@ -94,7 +145,7 @@ app.post('/', (req, res) => {
               name: 'mongodb',
             },
           ],
-          command: ['mongod','--auth', '--port', randomMPort.toString(), '--wiredTigerCacheSizeGB', wiredTigerCacheSizeGB.toString(), '--oplogSize', "50".toString()],
+          command: ['mongod', '--auth', '--port', randomMPort.toString(), '--wiredTigerCacheSizeGB', wiredTigerCacheSizeGB.toString(), '--oplogSize', "50".toString()],
           env: [
             {
               name: 'MONGO_INITDB_ROOT_USERNAME',
@@ -105,30 +156,18 @@ app.post('/', (req, res) => {
               value: data.pwd,
             },
           ],
-
-          resources: {
-            limits: {
-              cpu: '1',
-              memory: '512Mi',
+          volumeMounts: [
+            {
+              name: 'mongodb-storage',
+              mountPath: mountPath,
             },
-            requests: {
-              cpu: '0.5',
-              memory: '256Mi',
-            },
-          },
-		  volumes: [
-        {
-          name: 'mongo-persistent-storage',
-          persistentVolumeClaim: {
-            claimName: 'mongodb-db',
-          },
+          ],
         },
-      ],
-        },
+     
       ],
     },
   };
-
+  
 
 
 
@@ -136,40 +175,42 @@ app.post('/', (req, res) => {
 
 //defining service
 
-// const service = {
-//   apiVersion: 'v1',
-//   kind: 'Service',
-//   metadata: {
-//     name: data.cluster,
-//   },
-//   spec: {
-//     selector: {
-//       app: data.cluster,
-//     },
-//     ports: [
-//       {
-//         name: 'mongodb',
-//         port: randomMPort,
-//         targetPort: 'mongodb',
-//       },
-//     ],
-//   },
-// };
+const service = {
+  apiVersion: 'v1',
+  kind: 'Service',
+  metadata: {
+    name: data.cluster,
+  },
+  spec: {
+    selector: {
+      app: data.cluster,
+    },
+    ports: [
+      {
+        name: 'mongodb',
+        protocol: 'TCP',
+        port: randomMPort,
+        targetPort : randomMPort ,
+        nodePort: randomNPort
+      },
+    ],
+    type: 'LoadBalancer',
+  },
+};
 
-// coreV1Api.createNamespacedService('hackathon2023-mongo-t-mobile', service)
-//   .then((response) => {
-//     console.log(response.body);
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
+coreV1Api.createNamespacedService('hackathon2023-mongo-t-mobile', service)
+  .then((response) => {
+    console.log(response.body);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 // Create the pod
 coreV1Api.createNamespacedPod('hackathon2023-mongo-t-mobile', pod)
   .then((response) => {
-    console.log(response.body);
     
-
+   
     const username = kc.getCurrentUser();
     console.log('Current user:', username.name);
 
@@ -189,13 +230,17 @@ coreV1Api.createNamespacedPod('hackathon2023-mongo-t-mobile', pod)
 
 app.delete("/pods/:name", async (req, res) => {
   const podName = req.params.name;
- // const serviceName = req.params.name;
+  const serviceName = req.params.name;
 
   try {
     await coreV1Api.deleteNamespacedPod(podName, "hackathon2023-mongo-t-mobile");
+    console.log(`Pod ${podName} deleting...`);
+    await new Promise((resolve) => setTimeout(resolve, 5000));
     console.log(`Pod ${podName} deleted successfully.`);
 
     await coreV1Api.deleteNamespacedService(serviceName, "hackathon2023-mongo-t-mobile");
+    console.log(`Pod ${serviceName} deleting...`);
+    await new Promise((resolve) => setTimeout(resolve, 5000));
     console.log(`Service ${serviceName} deleted successfully.`);
 
     res.status(200).send({
@@ -212,6 +257,22 @@ app.delete("/pods/:name", async (req, res) => {
 
 
 
+
+
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
+
+
+//function
+
+async function gettingexternalIP(podName, namespace) {
+  try {
+    const response = await coreV1Api.readNamespacedService(podName, namespace);
+    const externalIP = response.body.status.loadBalancer.ingress[0].ip;
+    return externalIP;
+  } catch (error) {
+    console.error(error);
+    return ''; // Return an empty string or handle the error as needed
+  }
+}
